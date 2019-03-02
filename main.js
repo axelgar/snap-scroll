1b10a2de413626457c668ba797ff73baa27ac12b
Axel Garcia <axel.garcia94@gmail.com>
2019-03-02 10:55:59 +0100

    adding dinamic vh

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const main = () => {
-
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 }
 
 window.addEventListener('load', main);
